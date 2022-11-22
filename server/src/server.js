e31b1e85002644989819205ceade163102f92181
Nguyễn Trúc An <114329768+nguyentrucan@users.noreply.github.com>
2022-11-22 14:03:54 +0700

    first commit

---
+++ server/src/server.js
@@ -0,0 +1,16 @@
+const http = require('http');
+
+const app = require('./app');
+
+const {loadPlanetsData} = require('./models/planets.model');
+
+const PORT = process.env.PORT || 8000;
+
+const server = http.createServer(app);
+
+await loadPlanetsData();
+
+server.listen(PORT, () =>{
+    console.log(`Listening on port ${PORT}...`);
+});
+
