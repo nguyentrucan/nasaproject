63e53275b76ecc86f6d066962f3c977e1f51fafa
Nguyễn Trúc An <114329768+nguyentrucan@users.noreply.github.com>
2022-12-16 23:42:10 +0700

    nasa project

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # nasaproject
+# ĐỒ ÁN CÔNG NGHỆ THÔNG TIN
+# Sinh viên thực hiện:
+Nguyễn Trúc An - 20110454
+# Giảng viên hướng dẫn
+T.S Huỳnh Xuân Phụng
--- server/src/app.js
+++ server/src/app.js
@@ -1,14 +1,24 @@
+const path = require('path');
 const express = require('express');
 const cors = require('cors');
+const morgan = require('morgan');
 
-const planetsRouter = require('./routes/planets/planets.router');
+const api = require('./routes/api');
 
 const app = express();
 
 app.use(cors({
-    origin: 'http://locahost:3000',
+  origin: 'http://localhost:3000',
 }));
+app.use(morgan('combined'));
+
 app.use(express.json());
-app.use(planetsRouter);
+app.use(express.static(path.join(__dirname, '..', 'public')));
+
+app.use('/v1', api);
+
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
+});
 
 module.exports = app;
